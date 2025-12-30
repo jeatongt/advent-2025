df7fed14fea95c66311e4ec12e88f7213755d971
eegt <37492234+eegt@users.noreply.github.com>
2025-12-30 11:46:34 -0500

    TIL about zip as a way to rotate a list of lists (2d array)

--- Day06/mather-pt2.py
+++ Day06/mather-pt2.py
@@ -2,46 +2,45 @@
 import math
 import re
 
-# So what got me here is that the number of spaces in front of each operand matter.
-# So I'll need to parse out the operands differently.
+# Still doing it wrong. I need turn the whole sheet into a byte array
+# and then process each column as a byte array, not split by spaces
 
 def get_operands(worksheet, column):
-    return [int(row[column]) for row in worksheet]
+    return [int(number) for number in worksheet[column]]
 
-def join_item_to_precending_spaces(line_with_spaces):
-    joined_line = []
-    current_item = ""
-    for item in line_with_spaces:
-        if item.isspace():
-            current_item += item
-        else:
-            current_item += item
-            joined_line.append(current_item)
-            current_item = ""
-    return joined_line
-
-operands = []
-operators = []
-answers = []
-with open("Day06/homework_test.txt") as file:
+worksheet_unrotated = []
+with open("Day06/homework_actual.txt") as file:
     for line in file:
-        this_line = line.split()
-        if this_line[0] == "+" or this_line[0] == "*":
-            operators = this_line
+        maybe_operators = line.split()
+        if maybe_operators[0] == "+" or maybe_operators[0] == "*":
+            operators = maybe_operators
         else:
-            this_line_with_spaces = re.split(r'(\s+)', line.rstrip('\n'))
-            # print("Line with spaces:", this_line_with_spaces)
-            this_line_with_spaces_not_empty = [item for item in this_line_with_spaces if item != '']
-            # print("Line with spaces not empty:", this_line_with_spaces_not_empty)
-            operands.append(join_item_to_precending_spaces(this_line_with_spaces_not_empty))
+            this_line = list(line.rstrip('\n'))
+            worksheet_unrotated.append(this_line)
+# print(worksheet_unrotated)
+worksheet = [list(row) for row in zip(*worksheet_unrotated)]
+# print(worksheet)
+operands = []
+operand_row = []
+for row in worksheet:
+    joined_row = ''.join(row)
+    if joined_row.strip().isdigit():
+        operand_row.append(joined_row.strip())
+    else:
+        if operand_row:
+            operands.append(operand_row)
+            operand_row = []
+if operand_row:
+    operands.append(operand_row)
 print("Operators:", operators)
 print("Operands:", operands)
-# print("First column of operands:", get_operands(operands, 0))
+print("First column of operands:", get_operands(operands, 0))
 print(len(operators))
-# for i in range(len(operators)):
-#     print("Processing column", i)
-#     if operators[i] == "+":
-#         answers.append(sum(get_operands(operands, i)))
-#     elif operators[i] == "*":
-#         answers.append(math.prod(get_operands(operands, i)))
-# print("Total of answers:", sum(answers))
+answers = []
+for i in range(len(operators)):
+    print("Processing column", i)
+    if operators[i] == "+":
+        answers.append(sum(get_operands(operands, i)))
+    elif operators[i] == "*":
+        answers.append(math.prod(get_operands(operands, i)))
+print("Total of answers:", sum(answers))
