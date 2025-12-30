d25dbdf536945146d801e773d3105103804c288c
eegt <37492234+eegt@users.noreply.github.com>
2025-12-29 23:23:16 -0500

    still oomkilling. Gonna try math

--- Day05/fresh-finder-pt2.py
+++ Day05/fresh-finder-pt2.py
@@ -1,7 +1,8 @@
 # python
 
 fresh_ranges = []
-fresh_IDs = set()
+fresh_sets = []
+total_fresh_IDs = 0
 with open('Day05/db-actual.txt', 'r') as file:
     for line in file:
         range_parts = line.strip().split('-')
@@ -13,9 +14,25 @@ with open('Day05/db-actual.txt', 'r') as file:
             fresh_ranges.append((range_start, range_end))
         if len(range_parts) == 1:
             continue
+fresh_ranges.sort()
+print(f"Fresh ranges: {fresh_ranges}")
+
+this_fresh_set = set()
 for fresh_range in fresh_ranges:
-    fresh_IDs.update(range(fresh_range[0], fresh_range[1] + 1))
+    if not this_fresh_set:
+        this_fresh_set.update(range(fresh_range[0], fresh_range[1] + 1))
+    else:
+        if fresh_range[0] <= max(this_fresh_set) + 1:
+            this_fresh_set.update(range(fresh_range[0], fresh_range[1] + 1))
+            print(f"Merged fresh ID set: {this_fresh_set}")
+        else:
+            fresh_sets.append(this_fresh_set)
+            print(f"Finalized fresh ID set: {this_fresh_set}")
+            this_fresh_set = set()
+            this_fresh_set.update(range(fresh_range[0], fresh_range[1] + 1))
+fresh_sets.append(this_fresh_set)
+print(f"Finalized fresh ID set: {this_fresh_set}")
 
-print(f"Total fresh IDs: {len(fresh_IDs)}")
-
-
+for fresh_set in fresh_sets:
+    total_fresh_IDs += len(fresh_set)
+print(f"Total fresh IDs: {total_fresh_IDs}")
