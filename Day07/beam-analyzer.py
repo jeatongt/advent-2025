cf1f65803e201019079ffeb8fe608c2cb081c73c
eegt <37492234+eegt@users.noreply.github.com>
2025-12-30 19:29:55 -0500

    Hooray! I got a gold star

--- Day07/beam-analyzer.py
+++ Day07/beam-analyzer.py
@@ -13,10 +13,35 @@ def is_a_splitter(x, y, manifold):
     return False
 
 def beam_splits_next_row(x, y, manifold):
+    if y == len(manifold) - 1:
+        return False
     return is_a_splitter(x, y+1, manifold)
 
+def split_beam_next_row(x, y, manifold):
+    manifold[y+1][x-1] = '|'
+    manifold[y+1][x+1] = '|'
+
+def continue_beam_next_row(x, y, manifold):
+    if y == len(manifold) - 1:
+        return
+    manifold[y+1][x] = '|'
+
+def analyze_beam_path(manifold):
+    beam_splits = 0
+    for y in range(len(manifold)):
+        for x in range(len(manifold[0])):
+            if is_a_beam(x, y, manifold):
+                if beam_splits_next_row(x, y, manifold):
+                    split_beam_next_row(x, y, manifold)
+                    beam_splits += 1
+                else:
+                    continue_beam_next_row(x, y, manifold)
+    return beam_splits
+
 manifold = []
-with open('Day06/manifold-test.txt', 'r') as file:
+with open('Day07/manifold-actual.txt', 'r') as file:
     for line in file:
         manifold_line = list(line.strip())
         manifold.append(manifold_line)
+total_beam_splits = analyze_beam_path(manifold)
+print(f"Total beam splits: {total_beam_splits}")
